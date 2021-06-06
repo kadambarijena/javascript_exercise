55cbe90f3432a888b11d319f7d2f08e1e189a873
Kadambari Jena <pinkyjena1993@gmail.com>
2021-06-07 00:46:01 +0530

    2nd-project-quiz-app - Write function for quizapp - Write HTML - Added radio button

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -1,32 +1,58 @@
-var quizData = [
+const quizData = [
   {
-    Question: 'What is the first of computer?';
-    a: u
-    b: c
-    C: s
-    d: r
+    question: 'What is the first letter of computer?',
+    a: 'U',
+    b: 'C',
+    C: 'S',
+    d: 'R',
     correct: 'b'
   }, {
-    Question: 'How can you catch a computer virus?';
-    a: Sending e-mail messages
-    b: Using a laptop during the winter
-    c: Opening e-mail attachments
-    d: Shopping on-line
+    question: 'How can you catch a computer virus?',
+    a: 'Sending e-mail messages',
+    b: 'Using a laptop during the winter',
+    c: 'Opening e-mail attachments',
+    d: 'Shopping on-line',
     correct: 'c'
   }, {
-    Question: 'Google (www.google.com) is a? ';
-    a: Search Engine
-    b: url
-    c: Directory of images
-    d: Chat service on the web
+    question: 'Google (www.google.com) is a ?',
+    a: 'Search Engine',
+    b: 'url',
+    c: 'Directory of images',
+    d: 'Chat service on the web',
     correct: "a"
 
   }, {
-    Question: Which is not an Internet protocol?
-    a: HTTP
-    b: FTP
-    c: STP
-    d: IP
+    question: 'Which is not an Internet protocol?',
+    a: 'HTTP',
+    b: 'FTP',
+    c: 'STP',
+    d: 'IP',
     correct: "c"
   }
-]
+];
+
+const questionE1 = document.getElementById("question");
+const option_1 = document.getElementById("option_1");
+const option_2 = document.getElementById("option_2");
+const option_3 = document.getElementById("option_3");
+const option_4 = document.getElementById("option_4");
+
+//const loadQuiz = () => {
+//  question.innerText = quizData[0].question;
+//}
+//loadQuiz();
+let currentQuiz = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+  const currentQuizData = quizData[currentQuiz];
+
+  questionE1.innerText = question;
+  option_1.innerText = a;
+  option_2.innerText = b;
+  option_3.innerText = c;
+  option_4.innerText = d;
+
+  currentQuestion++;
+}
